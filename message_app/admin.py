bc13e5bd69cc13716d45cdf59f22dc2b36ea68e5
avlf <maxtusyuk@gmail.com>
2023-01-31 19:30:22 +0300

    adminka

--- message_app/admin.py
+++ message_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+    # def get_fields(self, request, obj=None):
+    # pass
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Message, MessageAdmin)
