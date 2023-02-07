c53229169ec8627802a4903d391fcecc0715f42e
avlf <maxtusyuk@gmail.com>
2023-02-07 19:33:58 +0300

    crispy add

--- message_servise/settings.py
+++ message_servise/settings.py
@@ -36,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'message_app',
-
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -113,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- message_servise/urls.py
+++ message_servise/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from message_servise import settings
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('message_app.urls')),
 
-)
+)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
